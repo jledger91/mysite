943b1557fe83d7149c416b85140030e060a68ab0
jledger91 <j.ford.91@hotmail.com>
2021-02-03 00:02:47 +0000

    Docstring for FilmAdmin.

--- backend/src/mysite/admin/film.py
+++ backend/src/mysite/admin/film.py
@@ -5,6 +5,8 @@ from mysite.models import Film
 
 @admin.register(Film)
 class FilmAdmin(admin.ModelAdmin):
+    """Admin class for the Film model."""
+
     list_display = (
         'title',
         'release_date',
