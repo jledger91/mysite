db93dea9fc0f0cb5ec06460b555771526db11775
jledger91 <j.ford.91@hotmail.com>
2021-02-05 01:31:24 +0000

    Removing email from base UserSerializer.

--- backend/src/api/serializers/user/user.py
+++ backend/src/api/serializers/user/user.py
@@ -13,7 +13,6 @@ class UserSerializer(serializers.ModelSerializer):
             'username',
             'first_name',
             'last_name',
-            'email',
             'is_staff',
         )
         read_only_fields = (
