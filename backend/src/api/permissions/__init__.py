078cf49262398257472f6d5a10ac9f6799a569a3
jledger91 <j.ford.91@hotmail.com>
2021-02-05 04:10:09 +0000

    Removing irrelevant permission.

--- backend/src/api/permissions/__init__.py
+++ backend/src/api/permissions/__init__.py
@@ -1,6 +1,5 @@
 from .is_staff_or_read_only import *
 from .is_superuser import *
-from .is_unauthenticated_and_is_create import *
 from .is_owner_or_read_only import *
 from .is_self_or_read_only import *
 from .is_staff_and_is_delete import *
