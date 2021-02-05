f57ccdb2accb481d82f0ed27522535daee2650d2
jledger91 <j.ford.91@hotmail.com>
2021-02-05 01:11:56 +0000

    Making posters generated by the FilmFactory a more representative size.

--- backend/src/mysite/factories/film.py
+++ backend/src/mysite/factories/film.py
@@ -10,7 +10,11 @@ class FilmFactory(factory.django.DjangoModelFactory):
         model = Film
 
     title = factory.Faker('sentence', nb_words=3)
-    poster = factory.django.ImageField(color='black')
+    poster = factory.django.ImageField(
+        color='black',
+        height=755,
+        width=510,
+    )
     release_date = factory.Faker('date_object')
     duration = factory.Faker('time')
     synopsis = factory.Faker('paragraph', nb_sentences=10)
