72dbd1ac74b85c497f2d012b7b1f26a781ee71f7
jledger91 <j.ford.91@hotmail.com>
2021-02-08 18:48:47 +0000

    Adding average score to film viewset/serializer/filters.

--- backend/src/api/views/viewsets/film.py
+++ backend/src/api/views/viewsets/film.py
@@ -1,3 +1,5 @@
+from django.db.models import Avg
+
 from django_filters.rest_framework import DjangoFilterBackend
 
 from rest_framework import viewsets
@@ -12,7 +14,9 @@ from mysite.models import Film
 class FilmViewSet(viewsets.ModelViewSet):
     """View set for the Film model."""
 
-    queryset = Film.objects.all()
+    queryset = Film.objects.annotate(
+        average_score=Avg('review__rating'),
+    )
     serializer_class = FilmSerializer
     permission_classes = [IsStaffOrReadOnly]
     filter_backends = [DjangoFilterBackend]
