f81f158bb4b98bf8e624de1d88aa9fe834c53643
jledger91 <j.ford.91@hotmail.com>
2021-02-14 15:39:03 +0000

    Adding read_only status to declared variables on ReviewSerializer

--- backend/src/api/serializers/review.py
+++ backend/src/api/serializers/review.py
@@ -6,9 +6,11 @@ from mysite.models import Review
 class ReviewSerializer(serializers.ModelSerializer):
     """Serializer for the Review model."""
 
-    username = serializers.CharField(source='user.username')
-    first_name = serializers.CharField(source='user.first_name')
-    last_name = serializers.CharField(source='user.last_name')
+    username = serializers.CharField(source='user.username', read_only=True)
+    first_name = serializers.CharField(
+        source='user.first_name', read_only=True
+    )
+    last_name = serializers.CharField(source='user.last_name', read_only=True)
 
     class Meta:
         model = Review
