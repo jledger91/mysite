170ad1b542c3123f9e7af7e480f2d2b9a070cfd8
jledger91 <j.ford.91@hotmail.com>
2021-02-15 19:07:34 +0000

    Adding backend favicon to stop django complaining about it not being found.

--- backend/src/project/settings/base.py
+++ backend/src/project/settings/base.py
@@ -142,6 +142,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'project', 'static'),
+]
 STATIC_ROOT = os.environ.get('STATIC_ROOT')
 STATIC_URL = '/static/'
 
