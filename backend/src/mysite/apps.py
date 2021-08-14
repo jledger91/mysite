4d790912ef65d9daf297540dbd149e5d80e90802
jledger91 <j.ford.91@hotmail.com>
2021-08-14 15:35:33 +0100

    Configuring deafult_auto_field parameter in MySiteConfig in response to warnings generated in tests.

--- backend/src/mysite/apps.py
+++ backend/src/mysite/apps.py
@@ -3,6 +3,7 @@ from django.apps import AppConfig
 
 class MySiteConfig(AppConfig):
     name = 'mysite'
+    default_auto_field = 'django.db.models.AutoField'
 
     def ready(self):
         super().ready()
